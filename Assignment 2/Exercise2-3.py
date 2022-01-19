f7221cbb1af88106af4286bad5d43c38b2aca76d
cruzandfamily <97995700+cruzandfamily@users.noreply.github.com>
2022-01-18 20:43:18 -0500

    Add files via upload

---
+++ Assignment 2/Exercise2-3.py
@@ -0,0 +1,6 @@
+hrs = input("enter hours: ")
+rt = input("enter rate: ")
+fh = float(hrs)
+fr = float(rt)
+pay = fh * fr
+print("pay: ", pay)
---
+++ Assignment 2/Exercise2-5.py
@@ -0,0 +1,4 @@
+cel = input("Celcius to Farenheit?")
+far = int(cel) * 1.8
+trfar = float(far) + 32
+print ("farenheit", trfar)
